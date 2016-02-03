b75095626d61c303ac698483d49cd0e91a3bb458
volucrisscr <quinn@volucris.space>
2016-02-03 12:43:31 -0500

    Add unit test stubs for first 2 QuizProcess funcs

---
+++ MondainRefactorTests/App_Code/QuizProcessTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MondainDeploy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MondainDeploy.Tests
+{
+    [TestClass()]
+    public class QuizProcessTests
+    {
+        //todo: Add ProcessQuestion test
+        //[TestMethod()]
+        //public void ProcessQuestionTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //todo: Add UpdateTotalSolutionsLabelWhenCorrect test
+        //[TestMethod()]
+        //public void UpdateTotalSolutionsLabelWhenCorrectTest()
+        //{
+        //    Assert.Fail();
+        //}
+    }
+}
