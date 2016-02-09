268b45b017b8f5b3e4881dfcf7c4bbf26ab3ac47
volucrisscr <quinn@volucris.space>
2016-02-08 19:49:30 -0500

    XML documentation for WordData struct

--- MondainDeploy/App_Code/WordData.cs
+++ MondainDeploy/App_Code/WordData.cs
@@ -1,5 +1,8 @@
 ﻿namespace MondainDeploy
 {
+    /// <summary>
+    /// Struct to represent the properties of a word.
+    /// </summary>
     public struct WordData
     { 
         public string Alphagram;
@@ -8,6 +11,14 @@
         public int Playability;
         public string Definition;
 
+        /// <summary>
+        /// Simple constructor, sets variables equal to input params.
+        /// </summary>
+        /// <param name="alphagram">The alphagram associated with the word.</param>
+        /// <param name="isNew">Whether the word was newly added to the lexicon in its most recent update.</param>
+        /// <param name="probability">Rank order of how likely the word is to be drawn in word games.</param>
+        /// <param name="playability">Rank order of how useful the word is in word games.</param>
+        /// <param name="definition">The meaning of the word.</param>
         public WordData(string alphagram, bool isNew, int probability, int playability, string definition)
         {
             Alphagram = alphagram;
